09fcb8792aa8286ac43ca825d03aaa202fd12a0a
liutaochange <liutaochange@163.com>
2020-04-28 18:03:19 +0800

    feat: 基础架构搭建

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { GlobalStyle } from './style'
+import { IconStyle } from 'Assets/iconfont/iconfont.js'
+import router from './router/index'
+import store from './store/index'
+import { renderRoutes } from 'react-router-config'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <IconStyle />
+        {renderRoutes(router)}
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
